81efce3ffa83369b957a3ebc049ccab614b4b1e9
Clayton Watts <cletusw@gmail.com>
2022-01-01 23:52:20 -0800

    Switch to module_init, module_exit

--- hello-1.c
+++ hello-1.c
@@ -1,4 +1,4 @@
-//#include <linux/init.h>
+#include <linux/init.h>
 #include <linux/kernel.h> /* Needed for pr_info() */
 #include <linux/module.h>
 
@@ -6,17 +6,21 @@ MODULE_DESCRIPTION("A sample driver");
 MODULE_AUTHOR("Clayton Watts <cletusw@gmail.com>");
 MODULE_LICENSE("Dual MIT/GPL");
 
-int init_module(void) {
+static int __init my_init(void) {
 	/* To show logs: */
 	/* sudo journalctl --since "5 minutes ago" | grep kernel */
-	/* Or tail ("wait on") just the info logs with: */
-	/* dmesg -w -l info */
+	/* Or tail ("wait on") the logs with: */
+	/* dmesg -w */
+	/* (optionally use `-l info` for just info level logs) */
 	pr_info("Hello, cletusw.\n");
 
 	/* A non 0 return means init_module failed; module can't be loaded. */
 	return 0;
 }
 
-void cleanup_module(void) {
+static void __exit my_exit(void) {
 	pr_info("Goodbye, cletusw.\n");
 }
+
+module_init(my_init);
+module_exit(my_exit);
