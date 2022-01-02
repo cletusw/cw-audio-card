6e19f64207b1d072560e27cad6e827591fc1bb19
Clayton Watts <cletusw@gmail.com>
2022-01-02 01:43:27 -0800

    Register as a sound driver

--- tas5756.c
+++ tas5756.c
@@ -2,6 +2,7 @@
 #include <linux/init.h>
 #include <linux/kernel.h> /* Needed for pr_info() */
 #include <linux/module.h>
+#include <sound/soc.h>
 
 MODULE_DESCRIPTION("Implementation of a TAS5756M driver for learning purposes");
 MODULE_AUTHOR("Clayton Watts <cletusw@gmail.com>");
@@ -10,6 +11,28 @@ MODULE_LICENSE("Dual MIT/GPL");
 /* Private data for the TAS5756 */
 struct tas5756_private {
 	int cool_data;
+};
+
+static const struct snd_kcontrol_new tas5756_snd_controls[] = {
+};
+
+static const struct snd_soc_dapm_widget tas5756_dapm_widgets[] = {
+};
+
+static const struct snd_soc_dapm_route tas5756_routes[] = {
+};
+
+static const struct snd_soc_component_driver soc_component_dev_tas5756 = {
+	.controls		= tas5756_snd_controls,
+	.num_controls		= ARRAY_SIZE(tas5756_snd_controls),
+	.dapm_widgets		= tas5756_dapm_widgets,
+	.num_dapm_widgets	= ARRAY_SIZE(tas5756_dapm_widgets),
+	.dapm_routes		= tas5756_routes,
+	.num_dapm_routes	= ARRAY_SIZE(tas5756_routes),
+	.idle_bias_on		= 1,
+	.use_pmdown_time	= 1,
+	.endianness		= 1,
+	.non_legacy_dai_naming	= 1,
 };
 
 static int tas5756_i2c_probe(struct i2c_client *client, const struct i2c_device_id *id)
@@ -21,7 +44,7 @@ static int tas5756_i2c_probe(struct i2c_client *client, const struct i2c_device_
 	/* Or tail ("wait on") the logs with: */
 	/* dmesg -w */
 	/* (optionally use `-l info` for just info level logs) */
-	pr_info("Hello from driver v1, cletusw!\n");
+	pr_info("Hello from driver v2, cletusw!\n");
 
 	tas5756 = devm_kzalloc(&client->dev, sizeof(*tas5756), GFP_KERNEL);
 	if (!tas5756)
@@ -29,7 +52,8 @@ static int tas5756_i2c_probe(struct i2c_client *client, const struct i2c_device_
 
 	i2c_set_clientdata(client, tas5756);
 
-	return 0;
+	return devm_snd_soc_register_component(
+			&client->dev, &soc_component_dev_tas5756, NULL, 0);
 }
 
 static int tas5756_i2c_remove(struct i2c_client *client)
