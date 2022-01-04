8b5af1d0e708a427c2f468e06c71e8603fc6a06d
Clayton Watts <cletusw@gmail.com>
2022-01-03 16:22:49 -0800

    Add stubbed ALSA control

--- tas5756.c
+++ tas5756.c
@@ -8,12 +8,40 @@ MODULE_DESCRIPTION("Implementation of a TAS5756M driver for learning purposes");
 MODULE_AUTHOR("Clayton Watts <cletusw@gmail.com>");
 MODULE_LICENSE("Dual MIT/GPL");
 
+#define TAS5756_VOLB 61
+#define TAS5756_VOLA 62
+
+#define MAX_VOLUME_VALUE 0xFE
+
 /* Private data for the TAS5756 */
 struct tas5756_private {
-	int cool_data;
+	unsigned int cool_data;
 };
 
+static inline unsigned int tas5756_read(struct snd_soc_component *component,
+	unsigned int reg)
+{
+	struct tas5756_private *tas5756 = snd_soc_component_get_drvdata(component);
+
+	pr_info("tas5756: hw read %d value 0x%x\n", reg, tas5756->cool_data);
+
+	return tas5756->cool_data;
+}
+
+static int tas5756_write(struct snd_soc_component *component, unsigned int reg,
+		unsigned int value)
+{
+	struct tas5756_private *tas5756 = snd_soc_component_get_drvdata(component);
+
+	pr_info("tas5756: hw write %d value 0x%x\n", reg, value);
+
+	tas5756->cool_data = value;
+
+	return 0;
+}
+
 static const struct snd_kcontrol_new tas5756_snd_controls[] = {
+	SOC_SINGLE("Hardware Master Playback Volume", TAS5756_VOLB, 0, MAX_VOLUME_VALUE, 1),
 };
 
 static const struct snd_soc_dapm_widget tas5756_dapm_widgets[] = {
@@ -23,16 +51,18 @@ static const struct snd_soc_dapm_route tas5756_routes[] = {
 };
 
 static const struct snd_soc_component_driver soc_component_dev_tas5756 = {
-	.controls		= tas5756_snd_controls,
-	.num_controls		= ARRAY_SIZE(tas5756_snd_controls),
-	.dapm_widgets		= tas5756_dapm_widgets,
-	.num_dapm_widgets	= ARRAY_SIZE(tas5756_dapm_widgets),
-	.dapm_routes		= tas5756_routes,
-	.num_dapm_routes	= ARRAY_SIZE(tas5756_routes),
-	.idle_bias_on		= 1,
-	.use_pmdown_time	= 1,
-	.endianness		= 1,
-	.non_legacy_dai_naming	= 1,
+	.write = tas5756_write,
+	.read = tas5756_read,
+	.controls = tas5756_snd_controls,
+	.num_controls = ARRAY_SIZE(tas5756_snd_controls),
+	.dapm_widgets = tas5756_dapm_widgets,
+	.num_dapm_widgets = ARRAY_SIZE(tas5756_dapm_widgets),
+	.dapm_routes = tas5756_routes,
+	.num_dapm_routes = ARRAY_SIZE(tas5756_routes),
+	.idle_bias_on = 1,
+	.use_pmdown_time= 1,
+	.endianness = 1,
+	.non_legacy_dai_naming = 1,
 };
 
 static int tas5756_i2c_probe(struct i2c_client *client, const struct i2c_device_id *id)
@@ -44,7 +74,7 @@ static int tas5756_i2c_probe(struct i2c_client *client, const struct i2c_device_
 	/* Or tail ("wait on") the logs with: */
 	/* dmesg -w */
 	/* (optionally use `-l info` for just info level logs) */
-	pr_info("Hello from driver v2, cletusw!\n");
+	pr_info("tas5756: Hello from driver v3, cletusw!\n");
 
 	tas5756 = devm_kzalloc(&client->dev, sizeof(*tas5756), GFP_KERNEL);
 	if (!tas5756)
@@ -52,13 +82,15 @@ static int tas5756_i2c_probe(struct i2c_client *client, const struct i2c_device_
 
 	i2c_set_clientdata(client, tas5756);
 
+	tas5756->cool_data = 3;
+
 	return devm_snd_soc_register_component(
 			&client->dev, &soc_component_dev_tas5756, NULL, 0);
 }
 
 static int tas5756_i2c_remove(struct i2c_client *client)
 {
-	pr_info("Goodbye, cletusw.\n");
+	pr_info("tas5756: Goodbye, cletusw.\n");
 
 	return 0;
 }
