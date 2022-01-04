7b9db1c5e1453d9028caae3a74815bd455e17ea1
Clayton Watts <cletusw@gmail.com>
2022-01-03 18:13:07 -0800

    Switch from manual i2c to regmap
    https://programmer.group/regmap-api-a-register-map-abstraction.html

--- tas5756.c
+++ tas5756.c
@@ -2,60 +2,51 @@
 #include <linux/init.h>
 #include <linux/kernel.h> /* Needed for pr_info() */
 #include <linux/module.h>
+#include <linux/regmap.h>
 #include <sound/soc.h>
 
 MODULE_DESCRIPTION("Implementation of a TAS5756M driver for learning purposes");
 MODULE_AUTHOR("Clayton Watts <cletusw@gmail.com>");
 MODULE_LICENSE("Dual MIT/GPL");
 
+// TAS5756 actually uses multiple pages of registers. Here we just assume page 0
 #define TAS5756_VOLL 61
 #define TAS5756_VOLR 62
 
-#define MAX_VOLUME_VALUE 0xFE
+#define TAS5756_MAX_REGISTER TAS5756_VOLR
+#define TAS5756_MAX_VOLUME_VALUE 0xFE
 
 /* Private data for the TAS5756 */
 struct tas5756_private {
-	struct i2c_client *i2c;
+	struct regmap *regmap;
 };
 
-static inline unsigned int tas5756_read(struct snd_soc_component *component,
-	unsigned int reg)
+static bool tas5756_readable_register(struct device *dev, unsigned int reg)
 {
-	struct tas5756_private *tas5756 = snd_soc_component_get_drvdata(component);
-	s32 response;
+	pr_info("tas5756: hw read %d", reg);
 
-	response = i2c_smbus_read_byte_data(tas5756->i2c, reg);
-
-	if (response < 0) {
-		pr_info("tas5756: ERROR: Failed hw read %d\n", reg);
-		return -EIO;
+	switch (reg) {
+	case TAS5756_VOLL ... TAS5756_VOLR:
+		return true;
+	default:
+		return false;
 	}
-
-	pr_info("tas5756: hw read %d value 0x%x\n", reg, response);
-
-	return response;
 }
 
-static int tas5756_write(struct snd_soc_component *component, unsigned int reg,
-		unsigned int value)
+static bool tas5756_writeable_register(struct device *dev, unsigned int reg)
 {
-	struct tas5756_private *tas5756 = snd_soc_component_get_drvdata(component);
-	s32 response;
+	pr_info("tas5756: hw write %d", reg);
 
-	response = i2c_smbus_write_byte_data(tas5756->i2c, reg, value);
-
-	if (response < 0) {
-		pr_info("tas5756: ERROR: Failed hw write %d\n value 0x%x\n", reg, value);
-		return -EIO;
+	switch (reg) {
+	case TAS5756_VOLL ... TAS5756_VOLR:
+		return true;
+	default:
+		return false;
 	}
-
-	pr_info("tas5756: hw write %d value 0x%x\n", reg, value);
-
-	return 0;
 }
 
 static const struct snd_kcontrol_new tas5756_snd_controls[] = {
-	SOC_DOUBLE_R("Hardware Master Playback Volume", TAS5756_VOLL, TAS5756_VOLR, 0, MAX_VOLUME_VALUE, 1),
+	SOC_DOUBLE_R("Hardware Master Playback Volume", TAS5756_VOLL, TAS5756_VOLR, 0, TAS5756_MAX_VOLUME_VALUE, 1),
 };
 
 static const struct snd_soc_dapm_widget tas5756_dapm_widgets[] = {
@@ -65,8 +56,6 @@ static const struct snd_soc_dapm_route tas5756_routes[] = {
 };
 
 static const struct snd_soc_component_driver soc_component_dev_tas5756 = {
-	.write = tas5756_write,
-	.read = tas5756_read,
 	.controls = tas5756_snd_controls,
 	.num_controls = ARRAY_SIZE(tas5756_snd_controls),
 	.dapm_widgets = tas5756_dapm_widgets,
@@ -79,16 +68,25 @@ static const struct snd_soc_component_driver soc_component_dev_tas5756 = {
 	.non_legacy_dai_naming = 1,
 };
 
+static const struct regmap_config tas5756_regmap = {
+	.reg_bits = 8,
+	.val_bits = 8,
+
+	.max_register = TAS5756_MAX_REGISTER,
+	.readable_reg = tas5756_readable_register,
+	.writeable_reg = tas5756_writeable_register,
+	.cache_type = REGCACHE_NONE,
+};
+
 static int tas5756_i2c_probe(struct i2c_client *client, const struct i2c_device_id *id)
 {
 	struct tas5756_private *tas5756;
+	int return_value;
 
-	/* To show logs: */
-	/* sudo journalctl --since "5 minutes ago" | grep kernel */
-	/* Or tail ("wait on") the logs with: */
+	/* To tail ("wait on") the logs: */
 	/* dmesg -w */
 	/* (optionally use `-l info` for just info level logs) */
-	pr_info("tas5756: Hello from driver v3, cletusw!\n");
+	pr_info("tas5756: Hello from driver v3!\n");
 
 	tas5756 = devm_kzalloc(&client->dev, sizeof(*tas5756), GFP_KERNEL);
 	if (!tas5756) {
@@ -96,7 +94,13 @@ static int tas5756_i2c_probe(struct i2c_client *client, const struct i2c_device_
 	}
 
 	i2c_set_clientdata(client, tas5756);
-	tas5756->i2c = client;
+
+	tas5756->regmap = devm_regmap_init_i2c(client, &tas5756_regmap);
+	if (IS_ERR(tas5756->regmap)) {
+		return_value = PTR_ERR(tas5756->regmap);
+		pr_info("tas5756: regmap_init() failed with: %d\n", return_value);
+		return return_value;
+	}
 
 	return devm_snd_soc_register_component(
 			&client->dev, &soc_component_dev_tas5756, NULL, 0);
@@ -104,7 +108,7 @@ static int tas5756_i2c_probe(struct i2c_client *client, const struct i2c_device_
 
 static int tas5756_i2c_remove(struct i2c_client *client)
 {
-	pr_info("tas5756: Goodbye, cletusw.\n");
+	pr_info("tas5756: Goodbye!\n");
 
 	return 0;
 }
