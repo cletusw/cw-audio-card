d62a281ff9f4124d11a4deee179d7ddb6fc33aeb
Clayton Watts <cletusw@gmail.com>
2022-02-19 11:27:25 +0000

    Update pins in scripts, remove pullup resistors for "out" pins in device tree

--- scripts/amp-enable.sh
+++ scripts/amp-enable.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Ignore errors, just means someone already set up this pin for access
-echo "25" > /sys/class/gpio/export 2> /dev/null
+echo "4" > /sys/class/gpio/export 2> /dev/null
 
 # Amp enable
-echo "out" > /sys/class/gpio/gpio25/direction
+echo "out" > /sys/class/gpio/gpio4/direction
 # Redundant since 0 is the default out value
-echo "0" > /sys/class/gpio/gpio25/value
+echo "0" > /sys/class/gpio/gpio4/value
 
 echo "Amp enabled. Run amp-unmute.sh"
 
